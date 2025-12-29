678a9b6a44c71be885f9fbe0f58e33b4e38bc96f
Samashi11 <maulashidqic10@gmail.com>
2025-12-29 11:03:15 +0700

    feat: Fix Bug Routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome')->name('home');
+    return view('welcome');
 });
 
 Route::get('/health', function () {
